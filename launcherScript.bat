8027428c47acb65bab51987798649218f3a1bbf2
marco <marco@pc>
2015-02-04 15:10:53 +0100

    Script to launch LMS by Code

---
+++ launcherScript.bat
@@ -0,0 +1,9 @@
+setlocal
+set ipaddr=192.168.0.06
+set server=G:\\Sviluppo\\slimserver\\slimserver.pl
+set squeezedir=C:\\Documents and Settings\\All Users\\Dati applicazioni\\SqueezeboxTest
+set prefsdir=%squeezedir%\\prefs
+set cachedir=%squeezedir%\\cache
+set logdir=%squeezedir%\\logs
+cmd.exe /c ""C:\\Perl\\bin\\perl.exe" "%server%" --playeraddr %ipaddr% --streamaddr %ipaddr% --httpaddr %ipaddr% --cliaddr %ipaddr% --prefsdir "%prefsdir%" --cachedir "%cachedir%" --logdir "%logdir%""
+ 
